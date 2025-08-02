70e58d08fe1c46b9b535091555be7fa45f9733bc
Masterkingplay <cristianricardogarciasaenz@gmail.com>
2025-08-02 17:14:20 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -292,6 +292,48 @@ end
  })
 
  local Button = Tab:CreateButton({
+    Name = "anti tp",
+    Callback = function()
+        local players = game:GetService('Players')
+local lplr = players.LocalPlayer
+local lastCF, stop, heartbeatConnection
+local function start()
+    heartbeatConnection = game:GetService('RunService').Heartbeat:Connect(function()
+        if stop then
+            return 
+        end 
+        lastCF = lplr.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame
+    end)
+    lplr.Character:FindFirstChildOfClass('Humanoid').RootPart:GetPropertyChangedSignal('CFrame'):Connect(function()
+        stop = true
+        lplr.Character:FindFirstChildOfClass('Humanoid').RootPart.CFrame = lastCF
+        game:GetService('RunService').Heartbeat:Wait()
+        stop = false
+    end)    
+    lplr.Character:FindFirstChildOfClass('Humanoid').Died:Connect(function()
+        heartbeatConnection:Disconnect()
+    end)
+end
+
+lplr.CharacterAdded:Connect(function(character)
+    repeat 
+        game:GetService('RunService').Heartbeat:Wait() 
+    until character:FindFirstChildOfClass('Humanoid')
+    repeat 
+        game:GetService('RunService').Heartbeat:Wait() 
+    until character:FindFirstChildOfClass('Humanoid').RootPart
+    start()
+end)
+
+lplr.CharacterRemoving:Connect(function()
+    heartbeatConnection:Disconnect()
+end)
+
+start()
+    end,
+ })
+
+ local Button = Tab:CreateButton({
     Name = "Vehicle fly",
     Callback = function()
         loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Vehicle%20Fly%20Gui'))()
